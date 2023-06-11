197a43c2455f9bf010406994674cac82b8764588
Owner <tench265@gmail.com>
2023-06-10 20:27:28 -0400

    core

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+rootProject.name = "OriginCore"
+
+setupOriginModule(
+    "origin-core",
+        listOf(
+            Pair("enchants", listOf("enchants-api", "enchants-impl")),
+            Pair("worldedit", listOf("v6", "v7"))));
+
+
+fun setupOriginModule(base: String, setup: List<Pair<String, List<String>>>) =
+    setup.forEach { pair
+        -> pair.second.forEach { name
+            -> setupSubproject(name, file("$base/${pair.first}/$name"))
+        }
+    }
+
+
+fun setupSubproject(name: String, projectDirectory: File) = setupSubproject(name) {
+    projectDir = projectDirectory
+}
+
+inline fun setupSubproject(name: String, block: ProjectDescriptor.() -> Unit) {
+    include(name)
+    project(":$name").apply(block)
+}
