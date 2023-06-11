ff68c5c6cea7c2589326bf9354b00a5007cc4b74
Owner <tench265@gmail.com>
2023-06-11 11:30:19 -0400

    PacketAPI and EnchantsAPI

---
+++ origin-core/enchants/enchants-impl/build.gradle.kts
@@ -0,0 +1,12 @@
+setupShadowJar()
+
+dependencies {
+    compileOnly(Dependencies.LOMBOK)
+    annotationProcessor(Dependencies.LOMBOK)
+    compileOnly(Dependencies.SPIGOT)
+    compileOnly(Dependencies.LUCKO_HELPER)
+    compileOnly(Dependencies.LUCKO_SQL)
+    implementation(project(":enchants-api"))
+    implementation(project(":packets-api"))
+
+}
---
+++ origin-core/server/packets-api/build.gradle.kts
@@ -0,0 +1,14 @@
+setupShadowJar()
+
+dependencies {
+    compileOnly(Dependencies.LOMBOK)
+    annotationProcessor(Dependencies.LOMBOK)
+    compileOnly(Dependencies.SPIGOT)
+    compileOnly(Dependencies.LUCKO_HELPER)
+    compileOnly(Dependencies.LUCKO_SQL)
+    implementation(Dependencies._SPIGOT)
+    compileOnly(Dependencies.GUICE)
+
+
+
+}
